5e1845a785a02fc1661142fc32865ad6cbe67a4a
KadoTomohiro <>
2021-01-15 18:33:08 +0900

    createt form page

---
+++ pages/form.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <form>
+      <div>
+        <label for="name">氏名</label
+        ><input id="name" type="text" /><span></span>
+      </div>
+      <div>
+        <label for="zipcode">郵便番号</label
+        ><input id="zipcode" type="text" /><span></span>
+      </div>
+      <div>
+        <label for="address">住所</label
+        ><input id="address" type="text" /><span></span>
+      </div>
+      <div>
+        <label for="contact"></label
+        ><input id="contact" type="text" /><span></span>
+      </div>
+      <div>
+        <label for="accept"></label
+        ><input id="accept" type="text" /><span></span>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style scoped>
+div {
+  color: red;
+}
+</style>
